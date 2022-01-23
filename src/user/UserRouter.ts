08f34849340a9357099e0f6c33f898fc46ddb0a1
BillyLuisNeedham <billyluisneedham@gmail.com>
2022-01-23 21:08:25 +0000

    UserRouter build out

--- src/user/UserRouter.ts
+++ src/user/UserRouter.ts
@@ -1,14 +1,29 @@
 import express from 'express'
+import { CreateUserUseCase } from '../../data'
+import { CreateUserRequest } from '../../domain'
+
+
 
 export const userRouter = express.Router()
 
 // TODO build out app.post /user, see it log in postman then hook into data layer
-userRouter.post('/', (req, res) => {
+userRouter.post('/', async (req, res) => {
     const body = req.body
     
     if (!body.name && !body.email) {
         res.status(400).send("invalid request body")
     }
 
-    res.send(body)
+    const useCase = new CreateUserUseCase()
+    const createUserReq: CreateUserRequest = {
+        name: body.name,
+        email: body.email
+    }
+    const result = await useCase.run(createUserReq)
+
+    if (!result.ok) {
+        res.status(500).send("something went wrong while creating user")
+    }
+
+    res.send(result.value)
 })
