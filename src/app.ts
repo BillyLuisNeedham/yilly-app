f6fb29aee58c619c82b8adac752024b7420c5b05
BillyLuisNeedham <billyluisneedham@gmail.com>
2022-01-23 17:44:39 +0000

    app.ts build out for post /user WIP

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 const app = express()
+const router = express.Router()
 const PORT = 3000
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+// TODO build out app.post /user, see it log in postman then hook into data layer
+// TODO refactor into own file when done building
+router.post('/', (req, res) => {
+    // const userRequest = req.body
+    // console.log(`userRequest from body: ${userRequest}`);
+    res.send("yo")
+})
+
+// TODO refactor route for user
+app.use('/', router)
 
 app.listen(PORT, () => {
     return console.log(`listening on ${PORT}`);
