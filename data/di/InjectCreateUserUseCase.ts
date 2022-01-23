bec3fdeb178ea6132089601dc7da29d7f83c0c27
BillyLuisNeedham <billyluisneedham@gmail.com>
2022-01-23 20:49:59 +0000

    InjectedCreateUserUseCase build out

--- data/di/InjectCreateUserUseCase.ts
+++ data/di/InjectCreateUserUseCase.ts
@@ -2,10 +2,11 @@ import { CreateUserUseCase as useCase } from '../../domain'
 import { UserRepository } from '../repositories'
 import { provideUserDataSource } from '../datasources'
 
+const injectedUserRepository =
+    new UserRepository(provideUserDataSource())
 
 export class CreateUserUseCase extends useCase {
-
     constructor() {
-        super(provideUserDataSource())
+        super(injectedUserRepository)
     }
 }
