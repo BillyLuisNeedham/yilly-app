e9a13c92be50038e52598011c4a384f1dbcfd537
BillyLuisNeedham <billyluisneedham@gmail.com>
2022-01-23 20:10:42 +0000

    UserRouter build out

--- src/user/UserRouter.ts
+++ src/user/UserRouter.ts
@@ -4,8 +4,6 @@ import { CreateUserRequest } from '../../domain'
 
 export const userRouter = express.Router()
 
-// TODO validation that req.body is a createUserRequest 
-
 // TODO build out app.post /user, see it log in postman then hook into data layer
 userRouter.post('/', (req, res) => {
     const body = req.body
@@ -14,7 +12,5 @@ userRouter.post('/', (req, res) => {
         res.status(400).send("invalid request body")
     }
 
-    console.log(`userRequest from body: `)
-    console.log(body)
     res.send(body)
 })
